64f0d22842555e437d647c37386ae71b2474ab6d
Zhang <zhang.hang@rutgers.edu>
2018-12-17 14:18:42 -0800

    bump version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ import setuptools.command.install
 
 cwd = os.path.dirname(os.path.abspath(__file__))
 
-version = '1.0.0'
+version = '1.0.1'
 try:
     sha = subprocess.check_output(['git', 'rev-parse', 'HEAD'], 
         cwd=cwd).decode('ascii').strip()
