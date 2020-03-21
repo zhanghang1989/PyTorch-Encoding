114c5e99413c9fd2e1d8d5ceb8a3aee25e37f6f4
Hang Zhang <zhang.hang@rutgers.edu>
2020-03-20 23:54:51 -0700

    mixup and label smoothing

--- experiments/recognition/train_dist.py
+++ experiments/recognition/train_dist.py
@@ -13,11 +13,8 @@ import time
 import argparse
 from tqdm import tqdm
 import torch.multiprocessing as mp
-try:
-    from mpi4py import MPI
-except ImportError:
-    logging.info('mpi4py is not installed. Use "pip install --no-cache mpi4py" to install')
-    MPI = None
+from mpi4py import MPI
+import numpy as np
 
 import torch
 import torch.nn as nn
@@ -37,13 +34,17 @@ class Options():
                             help='base image size')
         parser.add_argument('--crop-size', type=int, default=224,
                             help='crop image size')
+        parser.add_argument('--label-smoothing', type=float, default=0.0,
+                            help='label-smoothing (default eta: 0.0)')
+        parser.add_argument('--mixup', type=float, default=0.0,
+                            help='mixup (default eta: 0.0)')
         # model params 
         parser.add_argument('--model', type=str, default='densenet',
                             help='network model type (default: densenet)')
-        parser.add_argument('--pretrained', action='store_true', 
-                            default=False, help='load pretrianed mode')
         parser.add_argument('--rectify', action='store_true', 
                             default=False, help='rectify convolution')
+        parser.add_argument('--pretrained', action='store_true', 
+                            default=False, help='load pretrianed mode')
         # training hyper params
         parser.add_argument('--batch-size', type=int, default=128, metavar='N',
                             help='batch size for training (default: 128)')
@@ -65,7 +66,9 @@ class Options():
                             metavar='M', help='SGD momentum (default: 0.9)')
         parser.add_argument('--weight-decay', type=float, default=1e-4, 
                             metavar ='M', help='SGD weight decay (default: 1e-4)')
-        # cuda, seed and logging
+        parser.add_argument('--no-bn-wd', action='store_true', 
+                            default=False, help='no bias decay')
+        # seed
         parser.add_argument('--seed', type=int, default=1, metavar='S',
                             help='random seed (default: 1)')
         # checking point
@@ -139,23 +142,47 @@ def main_worker(gpu, ngpus_per_node, args):
     model_kwargs = {'pretrained': args.pretrained}
     if args.rectify:
         from encoding.nn import RFConv2d
-        #model_kwargs['conv_layer'] = RFConv2d
         model_kwargs['use_rfconv'] = True
 
     model = encoding.models.get_model(args.model, **model_kwargs)
     if args.gpu == 0:
         print(model)
 
-    criterion = nn.CrossEntropyLoss()
+    if args.mixup > 0:
+        train_loader = MixUpWrapper(args.mixup, 1000, train_loader, args.gpu)
+        criterion = NLLMultiLabelSmooth(args.label_smoothing)
+    elif args.label_smoothing > 0.0:
+        criterion = LabelSmoothing(args.label_smoothing)
+    else:
+        criterion = nn.CrossEntropyLoss()
 
     model.cuda(args.gpu)
     criterion.cuda(args.gpu)
     model = DistributedDataParallel(model, device_ids=[args.gpu])
 
     # criterion and optimizer
-    optimizer = torch.optim.SGD(model.parameters(), lr=args.lr,
-                                momentum=args.momentum,
-                                weight_decay=args.weight_decay)
+    if args.no_bn_wd:
+        parameters = model.named_parameters()
+        bn_params = [v for n, v in parameters if 'bn' in n] # or 'bias' in n
+        rest_params = [v for n, v in parameters if not 'bn' in n] #('bn' in n or 'bias' in n)
+        if args.gpu == 0:
+            print(" Weight decay NOT applied to BN parameters ")
+            #print(f'len(parameters): {len(model.parameters())} = {len(bn_params)} + {len(rest_params)}')
+        optimizer = torch.optim.SGD([{
+                                        'params': bn_params,
+                                        'weight_decay': 0
+                                    }, {
+                                        'params': rest_params,
+                                        'weight_decay': args.weight_decay
+                                    }],
+                                    lr=args.lr,
+                                    momentum=args.momentum,
+                                    weight_decay=args.weight_decay)
+    else:
+        optimizer = torch.optim.SGD(model.parameters(),
+                                    lr=args.lr,
+                                    momentum=args.momentum,
+                                    weight_decay=args.weight_decay)
     # check point
     if args.resume is not None:
         if os.path.isfile(args.resume):
@@ -184,18 +211,24 @@ def main_worker(gpu, ngpus_per_node, args):
         global best_pred, acclist_train
         for batch_idx, (data, target) in enumerate(train_loader):
             scheduler(optimizer, batch_idx, epoch, best_pred)
-            data, target = data.cuda(args.gpu), target.cuda(args.gpu)
+            if not args.mixup:
+                data, target = data.cuda(args.gpu), target.cuda(args.gpu)
             optimizer.zero_grad()
             output = model(data)
             loss = criterion(output, target)
             loss.backward()
             optimizer.step()
 
-            acc1 = accuracy(output, target, topk=(1,))
-            top1.update(acc1[0], data.size(0))
+            if not args.mixup:
+                acc1 = accuracy(output, target, topk=(1,))
+                top1.update(acc1[0], data.size(0))
+
             losses.update(loss.item(), data.size(0))
             if batch_idx % 100 == 0 and args.gpu == 0:
-                print('Batch: %d| Loss: %.3f | Top1: %.3f'%(batch_idx, losses.avg, top1.avg))
+                if args.mixup:
+                    print('Batch: %d| Loss: %.3f'%(batch_idx, losses.avg))
+                else:
+                    print('Batch: %d| Loss: %.3f | Top1: %.3f'%(batch_idx, losses.avg, top1.avg))
 
         acclist_train += [top1.avg]
 
@@ -213,28 +246,27 @@ def main_worker(gpu, ngpus_per_node, args):
                 top1.update(acc1[0], data.size(0))
                 top5.update(acc5[0], data.size(0))
 
-        if MPI is not None:
-            comm = MPI.COMM_WORLD
-            res1 = comm.gather(top1.avg, root=0)
-            res2 = comm.gather(top5.avg, root=0)
-            if args.gpu == 0:
-                top1_acc = sum(res1) / len(res1)
-                top5_acc = sum(res2) / len(res2)
-                print('Validation: Top1: %.3f | Top5: %.3f'%(top1_acc, top5_acc))
+        comm = MPI.COMM_WORLD
+        res1 = comm.gather(top1.avg, root=0)
+        res2 = comm.gather(top5.avg, root=0)
+        if args.gpu == 0:
+            top1_acc = sum(res1) / len(res1)
+            top5_acc = sum(res2) / len(res2)
+            print('Validation: Top1: %.3f | Top5: %.3f'%(top1_acc, top5_acc))
 
-        # save checkpoint
-        acclist_val += [top1.avg]
-        if top1.avg > best_pred:
-            best_pred = top1.avg 
-            is_best = True
-        encoding.utils.save_checkpoint({
-            'epoch': epoch,
-            'state_dict': model.module.state_dict(),
-            'optimizer': optimizer.state_dict(),
-            'best_pred': best_pred,
-            'acclist_train':acclist_train,
-            'acclist_val':acclist_val,
-            }, args=args, is_best=is_best)
+            # save checkpoint
+            acclist_val += [top1.avg]
+            if top1_acc > best_pred:
+                best_pred = top1_acc 
+                is_best = True
+            encoding.utils.save_checkpoint({
+                'epoch': epoch,
+                'state_dict': model.module.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'best_pred': best_pred,
+                'acclist_train':acclist_train,
+                'acclist_val':acclist_val,
+                }, args=args, is_best=is_best)
 
     for epoch in range(args.start_epoch, args.epochs + 1):
         tic = time.time()
@@ -243,6 +275,28 @@ def main_worker(gpu, ngpus_per_node, args):
         elapsed = time.time() - tic
         if args.gpu == 0:
             print(f'Epoch: {epoch}, Time cost: {elapsed}')
+
+class LabelSmoothing(nn.Module):
+    """
+    NLL loss with label smoothing.
+    """
+    def __init__(self, smoothing=0.1):
+        """
+        Constructor for the LabelSmoothing module.
+        :param smoothing: label smoothing factor
+        """
+        super(LabelSmoothing, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+
+    def forward(self, x, target):
+        logprobs = torch.nn.functional.log_softmax(x, dim=-1)
+
+        nll_loss = -logprobs.gather(dim=-1, index=target.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
 
 def accuracy(output, target, topk=(1,)):
     """Computes the accuracy over the k top predictions for the specified values of k"""
@@ -260,6 +314,64 @@ def accuracy(output, target, topk=(1,)):
             res.append(correct_k.mul_(100.0 / batch_size))
         return res
 
+class MixUpWrapper(object):
+    def __init__(self, alpha, num_classes, dataloader, device):
+        self.alpha = alpha
+        self.dataloader = dataloader
+        self.num_classes = num_classes
+        self.device = device
+
+    def mixup_loader(self, loader):
+        def mixup(alpha, num_classes, data, target):
+            with torch.no_grad():
+                bs = data.size(0)
+                c = np.random.beta(alpha, alpha)
+                perm = torch.randperm(bs).cuda()
+
+                md = c * data + (1-c) * data[perm, :]
+                mt = c * target + (1-c) * target[perm, :]
+                return md, mt
+
+        def expand(num_classes, tensor):
+            e = torch.zeros(tensor.size(0), num_classes)
+            e = e.scatter(1, tensor.unsqueeze(1), 1.0)
+            return e
+
+        for input, target in loader:
+            #target = expand(self.num_classes, target)
+            input, target = input.cuda(self.device), target.cuda(self.device)
+            target = torch.nn.functional.one_hot(target, self.num_classes)
+            i, t = mixup(self.alpha, self.num_classes, input, target)
+            yield i, t
+
+    def __len__(self):
+        return len(self.dataloader)
+
+    def __iter__(self):
+        return self.mixup_loader(self.dataloader)
+
+class NLLMultiLabelSmooth(nn.Module):
+    def __init__(self, smoothing = 0.1):
+        super(NLLMultiLabelSmooth, self).__init__()
+        self.confidence = 1.0 - smoothing
+        self.smoothing = smoothing
+
+    def forward(self, x, target):
+        if self.training:
+            x = x.float()
+            target = target.float()
+            logprobs = torch.nn.functional.log_softmax(x, dim = -1)
+    
+            nll_loss = -logprobs * target
+            nll_loss = nll_loss.sum(-1)
+    
+            smooth_loss = -logprobs.mean(dim=-1)
+    
+            loss = self.confidence * nll_loss + self.smoothing * smooth_loss
+    
+            return loss.mean()
+        else:
+            return torch.nn.functional.cross_entropy(x, target)
 
 class AverageMeter(object):
     """Computes and stores the average and current value"""
